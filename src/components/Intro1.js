14dbb0446ae634738c1b207e8bbffda3ccf6d70d
agustina12 <gustina200@gmail.com>
2023-07-20 15:14:12 +0800

    my first commit

---
+++ src/components/Intro1.js
@@ -0,0 +1,21 @@
+function Intro1() {
+  return (
+    <main className="main-hero">
+      <article>
+        <h1>
+          Get Connected to the Best Remote
+          <br /> Jobs in Your Field{" "}
+        </h1>
+        <p>
+          Discover a wide range of remote opportunities on our platform and take
+          control of your career
+        </p>
+        <div className="button-group">
+          <button>Explore Remote Jobs</button>
+          <a href="#">How it works?</a>
+        </div>
+      </article>
+    </main>
+  );
+}
+export default Intro1;
---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+function Nav() {
+  return (
+    <nav className="main-nav">
+      <div>
+        <h3>Jobless</h3>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Discover</li>
+          <li>Post a Job</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div>
+        <button>Sing Up</button>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
